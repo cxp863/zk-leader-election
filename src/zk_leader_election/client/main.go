f90ff57c523740d8a0ad87c6e379fd0edf907349
Hsinpeng Chen <cxp863@gmail.com>
2023-04-30 14:15:56 +0800

    single thread test

--- src/zk_leader_election/client/main.go
+++ src/zk_leader_election/client/main.go
@@ -5,21 +5,44 @@ import (
 	"fmt"
 	"google.golang.org/grpc"
 	"log"
+	"sync"
+	"sync/atomic"
+	"time"
 	idlsrv "zk-leader-election/src/zk_leader_election/idl/service"
 )
 
 func main() {
-
-	conn, err := grpc.Dial("localhost:10086", grpc.WithInsecure())
+	conn, err := grpc.Dial("192.168.2.205:10086", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Could not connect: %v", err)
 	}
 	defer conn.Close()
-	c := idlsrv.NewGreetServiceClient(conn)
+	client := idlsrv.NewGreetServiceClient(conn)
 
-	resp, err := c.Greeting(context.Background(), &idlsrv.GreetRequest{Message: "hello"})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(resp.Message)
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*1)
+	value := send(ctx, client)
+	fmt.Println(value)
+
+}
+
+func send(ctx context.Context, c idlsrv.GreetServiceClient) int64 {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	var value = atomic.Int64{}
+	go func(wg *sync.WaitGroup) {
+		for {
+			select {
+			case <-ctx.Done():
+				wg.Done()
+				return
+			default:
+				_, err := c.Greeting(ctx, &idlsrv.GreetRequest{Message: "hello"})
+				if err == nil {
+					value.Add(1)
+				}
+			}
+		}
+	}(&wg)
+	wg.Wait()
+	return value.Load()
 }
